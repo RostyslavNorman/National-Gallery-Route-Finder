c4e14ba27041a61290f8927d1f8bdce4852750c4
Christopher Bolger <20111314@setu.ie>
2026-03-20 15:05:52 +0000

    WIP

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -6,6 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
+    Stage primaryStage;
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/Application.fxml"));
+        Scene scene = new Scene(loader.load());
+        this.primaryStage = stage;
+        primaryStage.setTitle("London Museum");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
