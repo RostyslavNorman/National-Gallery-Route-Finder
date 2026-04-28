3eca05f5c5543562efada99eb806abc75c8857c2
chris <20111314@mail.wit.ie>
2026-04-28 23:49:49 +0100

    Will be used for BFS search algorithm

---
+++ src/main/java/algorithms/BFS.java
@@ -0,0 +1,15 @@
+package algorithms;
+
+import javafx.scene.Node;
+
+import javax.imageio.ImageReader;
+import javax.imageio.ImageWriter;
+
+public final class BFS {
+    ImageReader reader;
+    ImageWriter writer;
+
+    public static void BFS(int x, int y) {
+
+    }
+}
